31de4b133e33bdc0704216cb70c203ee47bbbeff
Julien LE THENO <Julien@Macbook-de-Julien.local>
2020-02-06 12:33:24 +0100

    [Fix] Fixing syntax

--- examples/fizzbuzz/fizzbuzz.go
+++ examples/fizzbuzz/fizzbuzz.go
@@ -1,4 +1,4 @@
-/** 
+/**
  *  This is a simple example on how to use the state machine framework in your project.
  *  This example declares the state machine statically in the code.
  *  Do you know Fizzbuzz ? It is a simple game where you count numbers and you need to say "fizz"
@@ -6,7 +6,7 @@
  *  If it is both a multiple of 3 and 5, you should say fizzbuzz.
  *
  *  We will modelize the game as a state machine described as so :
- * 
+ *
  * (Fizz state) --> (Buzz state)
  *         ^          ^
  *         v          V
@@ -29,34 +29,35 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    sm "go/state_machine"
+	"fmt"
+	"time"
+
+	sm "github.com/lethenju/state_machine_framework"
 )
 
-// Our counter 
-var counter = 0;
+// Our counter
+var counter = 0
 
 /** The runtime function is the heart of the State Machine. You need this minimal code to make the framework run
  *
- *  
+ *
  func runtime(s *sm.State) { // The parameter is the entry state of the SM.
 	for {
-	        // Launch the user-defined core function of the state 
-		s.Core_function()
+	        // Launch the user-defined core function of the state
+		s.CoreFunction()
 		// Transition to another state if possible (the state function will take care of everything)
-		s = (*s).State_function()
+		s = (*s).StateFunction()
 	}
 }
  *
- */
+*/
 func runtime(s *sm.State) {
 	for {
-		s.Core_function()
+		s.CoreFunction()
 		fmt.Println("Counter = ", counter)
 		time.Sleep(500 * time.Millisecond)
 
-		s = (*s).State_function()
+		s = (*s).StateFunction()
 	}
 }
 
@@ -64,89 +65,87 @@ func runtime(s *sm.State) {
  */
 func main() {
 	fmt.Println("Welcome to Fizz-Buzz Demo!")
-        
+
 	// Declaring normal state
-	var normal_state sm.State
-	normal_state.Name = "Normal"
-	normal_state.Core_function = func() {
+	var normalState sm.State
+	normalState.Name = "Normal"
+	normalState.CoreFunction = func() {
 		fmt.Println(".")
 		counter++
 	}
-	
-	var fizz_state  sm.State
-	fizz_state.Name = "Fizz"
-	fizz_state.Core_function = func() {
+
+	var fizzState sm.State
+	fizzState.Name = "Fizz"
+	fizzState.CoreFunction = func() {
 		fmt.Println(" == FIZZ == ")
 	}
 
-	var buzz_state  sm.State
-	buzz_state.Name = "Buzz"
-	buzz_state.Core_function = func() {
+	var buzzState sm.State
+	buzzState.Name = "Buzz"
+	buzzState.CoreFunction = func() {
 		fmt.Println(" == BUZZ == ")
 	}
 
 	// Describing connection from normal state to fizz state
-	normal_state.Connected   = append(normal_state.Connected, 
-		sm.Connection{ Connection_state : &fizz_state,
-			Reason_to_move : func () bool { 
-					if (counter % 3 == 0) {
-						return true
-					}
-					return false 
-				},
-				Transition : func () {
-					fmt.Println("[Normal] -> [Fizz]")
-				}},
-	// Describing connection from normal state to buzz state
-		sm.Connection{ Connection_state : &buzz_state,
-			Reason_to_move : func () bool { 
-					if (counter % 5 == 0) {
-						return true
-					}
-					return false 
-				},
-				Transition : func () {
-					fmt.Println("[Normal] -> [Buzz]")
-				}},
-	// Describing connection from normal state to normal state
-		sm.Connection{ Connection_state : &normal_state,
-			Reason_to_move : func () bool { return true },
-			    Transition : func () {
-					fmt.Println("[Normal] -> [Normal]")
-				}});
-
-	// Describing connection from fizz to buzz
-	fizz_state.Connected = append(fizz_state.Connected,
-		sm.Connection{ Connection_state : &buzz_state,
-			Reason_to_move : func () bool { 
-				if (counter % 5 == 0) {
+	normalState.Connected = append(normalState.Connected,
+		sm.Connection{ConnectionState: &fizzState,
+			ReasonToMove: func() bool {
+				if counter%3 == 0 {
 					return true
 				}
-				return false 
+				return false
 			},
-			Transition : func () {
+			Transition: func() {
+				fmt.Println("[Normal] -> [Fizz]")
+			}},
+		// Describing connection from normal state to buzz state
+		sm.Connection{ConnectionState: &buzzState,
+			ReasonToMove: func() bool {
+				if counter%5 == 0 {
+					return true
+				}
+				return false
+			},
+			Transition: func() {
+				fmt.Println("[Normal] -> [Buzz]")
+			}},
+		// Describing connection from normal state to normal state
+		sm.Connection{ConnectionState: &normalState,
+			ReasonToMove: func() bool { return true },
+			Transition: func() {
+				fmt.Println("[Normal] -> [Normal]")
+			}})
+
+	// Describing connection from fizz to buzz
+	fizzState.Connected = append(fizzState.Connected,
+		sm.Connection{ConnectionState: &buzzState,
+			ReasonToMove: func() bool {
+				if counter%5 == 0 {
+					return true
+				}
+				return false
+			},
+			Transition: func() {
 				fmt.Println("[Fizz] -> [Buzz]")
 			}},
-	// Describing connection from fizz to normal
-		sm.Connection{ Connection_state : &normal_state,
-			Reason_to_move : func () bool { 
+		// Describing connection from fizz to normal
+		sm.Connection{ConnectionState: &normalState,
+			ReasonToMove: func() bool {
 				return true
 			},
-			Transition : func () {
+			Transition: func() {
 				fmt.Println("[Fizz] -> [Normal]")
-			}});
-
+			}})
 
 	// Buzz to normal
-	buzz_state.Connected = append(buzz_state.Connected,
-		sm.Connection{ Connection_state : &normal_state,
-			Reason_to_move : func () bool { 
+	buzzState.Connected = append(buzzState.Connected,
+		sm.Connection{ConnectionState: &normalState,
+			ReasonToMove: func() bool {
 				return true
 			},
-			Transition : func () {
+			Transition: func() {
 				fmt.Println("[Buzz] -> [Normal]")
-			}});
+			}})
 
-
-	runtime(&normal_state)
+	runtime(&normalState)
 }
