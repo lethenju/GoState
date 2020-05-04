ce58df7d9ed2a71728844a147a66c15c54bce908
Julien LE THENO <julien.letheno@gmail.com>
2020-05-04 15:09:16 +0200

    [Add] begin to implement user structure

--- examples/manager/manager.go
+++ examples/manager/manager.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
-
+	"strings"
 	sm "github.com/lethenju/state_machine_framework"
 )
 
@@ -19,26 +19,79 @@ func runtime(s *sm.State) {
 }
 
 
+type stateMachine struct {
+	states []State;
+}
+
+
 func main() {
 	fmt.Println("Welcome to Manager!")
-	var running_flag = true;
-	while (running_flag) {
-		/** TODO Get user command
-		 -> Create variable (name, value)
+	var runningFlag = true
+	for (runningFlag) {
 
-		 -> Create state (Name)
-		 -> Add variable change in state
+		var userInput string
+		fmt.Print("> ")
+		fmt.Scan(&userInput)
+		userInput = strings.ToUpper(userInput)
+		command := strings.Split(userInput, " ")
+		switch (command[0]) {
+		case "CREATE":
+			switch (command[1]) {
+				
+		 //-> Create variable (name, value)
+			case "VARIABLE":
+				//TODO Add variable command[2] with optional value command[3]
+				break
+				
+		 //-> Create state (Name)
+			case "STATE":
+				//TODO Add state command[2]
+				break
+		 //-> Create Transition (Transition Name, From state, To state)
+			case "TRANSITION":
+				// Verify command[3] and command[4] are in the list of states
+				// Todo add transition command[2] to the state command[3]
+				// command[4]
+				break
+			}
+		case "ADD":
+			switch (command[1]) {
+			
+			/*-> Add variable change in state
 				(State Name, Entering/Leaving, VAR, OPERATION, VALUE or VAR)			 
-						Variable change include :
-							=  	(VAR) (INCR) (VALUE or VAR) // incrementation
-							=  	(VAR) (=) (VALUE or VAR) // affectation
-							
-		 -> Create Transition (Transition Name, From state, To state)
-		 -> Add Reason to move
-		 		(Transition Name, VAR, COMPARISON, VALUE or VAR)
+					Variable change include :
+						=  	(VAR) (INCR) (VALUE or VAR) // incrementation
+						=  	(VAR) (=)    (VALUE or VAR) // affectation */
+			case: "STATE_BEHAVIOUR":
+				// TODO Verify command in command[3] command[4] command[5]
+				// command[3] must be in variables
+				// command[4] must be in commandlist
+				// command[5] must be in variable OR is possible to cast as int
+				
+				switch (command[2]) {
+				case "ENTERING":
+					// TODO Add command in core function of state
+					break;
+				case "LEAVING":
+					// TODO Add command in ?
+					break;
+				}
+				break;
+			
+		    /*-> Add Reason to move
+		 		(State from, State to, VAR, COMPARISON, VALUE or VAR)
 					    A reason to move is based on variables
 						   = when (VAR) > (VALUE or VAR) 
-						   = when (VAR) == (VALUE or VAR)
+						   = when (VAR) == (VALUE or VAR)*/
+			case: "REASON_TO_MOVE":
+				// TODO verify in all states if the transition exists
+				// if not create it
+			}
+		}
+
+		/** TODO Get user command
+
+
 		 -> Display states
 					- State Name 
 							In : (VAR) (OPERATION) (VAR OR VALUE)
