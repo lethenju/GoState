a30960f488ba92c8d97689a376770ade82eb588d
Julien LE THENO <julien.letheno@gmail.com>
2020-05-04 15:44:14 +0200

    [Fix] Linter installed :)

--- examples/manager/manager.go
+++ examples/manager/manager.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"strings"
+	"time"
+
 	sm "github.com/lethenju/state_machine_framework"
 )
 
@@ -21,63 +22,65 @@ func runtime(s *sm.State) {
 // A used defined variable
 // Can be a string or an int, but the value is always stored as a string
 type Variable struct {
-	name  string 
-	value string
+	Name  string
+	Value string
 }
 
 // The state machine representation
 type stateMachine struct {
-	states []sm.State
-	variables []Variable
+	States    []sm.State
+	Variables []Variable
 }
-
 
 func main() {
 	fmt.Println("Welcome to Manager!")
 
-	var machine stateMachine;
+	var machine stateMachine
+
 	var runningFlag = true
-	for (runningFlag) {
+	for runningFlag {
 
 		var userInput string
 		fmt.Print("> ")
 		fmt.Scan(&userInput)
 		userInput = strings.ToUpper(userInput)
 		command := strings.Split(userInput, " ")
-		switch (command[0]) {
+		switch command[0] {
 		case "CREATE":
-			switch (command[1]) {
-				
-				//-> Create variable (name, value)
+			switch command[1] {
+			//-> Create variable (name, value)
 			case "VARIABLE":
+				machine.Variables = append(machine.Variables, Variable{
+					Name:  command[2],
+					Value: command[3]})
 				//TODO Add variable command[2] with optional value command[3]
 				break
-				
-		 //-> Create state (Name)
+
+			//-> Create state (Name)
 			case "STATE":
 				//TODO Add state command[2]
 				break
-		 //-> Create Transition ( From state, To state)
+			//-> Create Transition ( From state, To state)
 			case "TRANSITION":
 				// Verify command[2] and command[3] are in the list of states
 				// Todo add transition  to the state command[2]
 				break
 			}
 		case "ADD":
-			switch (command[1]) {
-			
+			switch command[1] {
+
 			/*-> Add variable change in state
-				(State Name, Entering/Leaving, VAR, OPERATION, VALUE or VAR)			 
-					Variable change include :
-						=  	(VAR) (INCR) (VALUE or VAR) // incrementation
-						=  	(VAR) (=)    (VALUE or VAR) // affectation */
+			(State Name, Entering/Leaving, VAR, OPERATION, VALUE or VAR)
+				Variable change include :
+					=  	(VAR) (INCR) (VALUE or VAR) // incrementation
+					=  	(VAR) (=)    (VALUE or VAR) // affectation */
 			case "STATE_BEHAVIOUR":
 				// TODO Verify command in command[3] command[4] command[5]
 				// command[3] must be in variables
 				// command[4] must be in commandlist
 				// command[5] must be in variable OR is possible to cast as int
-				
-				switch (command[2]) {
+
+				switch command[2] {
 				case "ENTERING":
 					// TODO Add command in core function of state
 					break
@@ -86,20 +89,20 @@ func main() {
 					break
 				}
 				break
-			
-		    /*-> Add Reason to move
-		 		(State from, State to, VAR, COMPARISON, VALUE or VAR)
-					    A reason to move is based on variables
-						   = when (VAR) > (VALUE or VAR) 
-						   = when (VAR) == (VALUE or VAR)*/
+
+				/*-> Add Reason to move
+				 		(State from, State to, VAR, COMPARISON, VALUE or VAR)
+							    A reason to move is based on variables
+								   = when (VAR) > (VALUE or VAR)
+								   = when (VAR) == (VALUE or VAR)*/
 			case "REASON_TO_MOVE":
 				// TODO verify in all states if the transition exists
 				// if not create it
-			break
+				break
 			}
-		/*	
+		/*
 			-> Display states
-			- State Name 
+			- State Name
 					In : (VAR) (OPERATION) (VAR OR VALUE)
 						  ...
 					Out : (VAR) (OPERATION) (VAR OR VALUE)
@@ -107,18 +110,18 @@ func main() {
 				Transitions In :
 					Name transitions
 					..
-				Transition Out : 
+				Transition Out :
 					Name transitions
 					..*/
 		case "DISPLAY":
 			// Todo display state
 			break
 		case "RUN":
-			switch (command[1]) {
-				case "MANUAL":
-					break
-				case "AUTO":
-					break
+			switch command[1] {
+			case "MANUAL":
+				break
+			case "AUTO":
+				break
 			}
 		}
 
